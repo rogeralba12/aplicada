ed28ed58bef69db0b8940fb081d2cccfef0ad7f4
xmasterpumax <xmasterpumax1234@hotmail.com>
2020-01-16 04:25:17 -0800

    Add files via upload

---
+++ eljuego/eljuego/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+
+namespace eljuego
+{
+	class Program
+	{
+
+		static game currentgame = new game();
+		static Thread inputThread;
+		
+		static void Main(string[] args)
+		{
+
+
+			
+			
+		currentgame.Gameinit();
+		inputThread = new Thread(input);
+		inputThread.Start();
+			while(currentgame.currentstate != game.egamestate.over)
+        {
+
+            switch (currentgame.currentstate)
+            {
+				case game.egamestate.starting:
+					Console.WriteLine("Digite un valor entre 1 y 1000:");
+						currentgame.currentstate = game.egamestate.playing;
+						break;
+				case game.egamestate.playing:
+					if (currentgame.Lasttry == 0)
+						continue;
+
+					switch (currentgame.Checkifguessed()) {
+						case game.attemptresult.greater:
+							Console.WriteLine("El numero secreto es menor");
+							break;
+						case game.attemptresult.lower:
+							Console.WriteLine("El numero secreto es mayor");
+							break;
+							default:
+						Console.WriteLine("Ha adivinao'");
+							currentgame.currentstate = game.egamestate.over;
+							break;
+
+					}
+
+
+					if(currentgame.currentstate != game.egamestate.over)
+						Console.WriteLine("dIGITE OTRO VALOR");
+
+					currentgame.Lasttry = 0;
+					break;
+            }
+
+        }
+		
+		
+
+		
+
+	//inputThread.Abort();
+	//inputThread.Join();
+			
+		Console.WriteLine("Gracias por jugar");
+	}
+
+		static void input()
+{
+
+	int currentvalue = 0;
+	while (currentgame.currentstate != game.egamestate.over)
+	{
+				
+		currentvalue = Convert.ToInt32(Console.ReadLine());
+		currentgame.Lasttry = currentvalue;
+	}
+
+
+}
+    }
+}
---
+++ eljuego/eljuego/game.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eljuego
+{
+    class game
+    {
+       
+
+
+
+        #region "enums"
+        public enum egamestate
+        {
+
+            starting, playing, over
+        }
+        public enum attemptresult
+        {
+
+            guessed,
+            lower,
+            greater
+        }
+
+        #endregion
+
+
+        #region "attributes"
+
+        const int MIN = 1, MAX = 1001;
+        const string DEFAULTPATH = "score.txt";
+        public int secretnumber { get; set; }
+        public int Lasttry { get; set; }
+        //random 1y 1000
+        
+        public int attempts { get; set; }
+        public egamestate currentstate { get; set; }
+        public DateTime starttime { get; set; }
+        public DateTime endtime { get; set; }
+        public TimeSpan TimeSpent
+        {
+
+            get
+            {
+                return endtime.Subtract(starttime);
+            }
+        }
+
+        public string scorepath { get; set; }
+
+        #endregion
+
+        #region "behaviours"
+        public void Gameinit()
+        {
+
+            scorepath = DEFAULTPATH;
+            secretnumber = generate(MIN, MAX);
+
+            currentstate = egamestate.starting;
+            attempts = 0;
+            starttime = DateTime.Now;
+            Console.WriteLine(read());
+        }
+        private int generate(int min, int max)
+        {
+
+
+            Random rnd = new Random();
+            return rnd.Next(min, max);
+        }
+
+        public attemptresult Checkifguessed()
+        {
+
+            attempts++;
+            if (Lasttry == secretnumber)
+            {
+                savestate();
+                return attemptresult.guessed;
+            }
+            else if (Lasttry > secretnumber)
+           
+                return attemptresult.greater;
+            
+          
+
+
+                return attemptresult.lower;
+            
+        }
+
+
+        public void savestate()
+        {
+
+
+            var streamwrite = File.AppendText(scorepath);
+            streamwrite.Write($"Tiempo: {TimeSpent.TotalSeconds} - Intentos: {attempts}");
+            streamwrite.Flush();
+            streamwrite.Close();
+        }
+
+        public string read()
+        {
+            if (File.Exists(scorepath))
+                return File.ReadAllText(scorepath);
+            else
+                return "";
+        }
+        #endregion
+
+    }
+
+        
+
+    
+
+    
+}
